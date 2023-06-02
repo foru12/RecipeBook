8a0ebe1f0fe3bb0d21fc49a0e4f907cc80ecc074
Thorin Oakenshield <kostya1705@gmail.com>
2023-06-02 22:30:16 +0300

    Add First commit

---
+++ app/src/main/java/com/example/bookrecipe/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bookrecipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val s = ""
+        val ass = ""
+        val s2 = ""
+        val s3 = ""
+        val s4 = ""
+        val s5 = ""
+        val s6= ""
+    }
+}
